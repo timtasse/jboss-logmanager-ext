06fd7ee9fef503ec8436cd9c56bd5457c61a56b8
timtasse <timtasse@users.noreply.github.com>
2016-01-04 10:40:55 +0100

    Zipping and deletion now in an extra-thread, not blocking the logging too long

--- src/main/java/org/jboss/logmanager/ext/handlers/ZippedPeriodicRotatingFileHandler.java
+++ src/main/java/org/jboss/logmanager/ext/handlers/ZippedPeriodicRotatingFileHandler.java
@@ -61,124 +61,11 @@ public class ZippedPeriodicRotatingFileHandler extends PeriodicRotatingFileHandl
         final String oldSuffix = this.getNextSuffix();
         super.preWrite(record);
         if (this.getNextSuffix() != null && !this.getNextSuffix().equals(oldSuffix)) {
-            this.zippingOldLog(oldSuffix);
-            this.checkMaxBackups();
+            final JobRunner jobRunner = new JobRunner(oldSuffix);
+            new Thread(jobRunner).start();
         }
     }
 
-    /**
-     * Check if the maximum number of backups is reached and delete the files that are too much
-     */
-    private void checkMaxBackups() {
-        if (this.maxBackups < 1) {
-            return;
-        }
-        final File file = this.getFile();
-        final File[] listFiles = file.getParentFile().listFiles(new FilenameFilter() {
-            @Override
-            public boolean accept(final File dir, final String name) {
-                return name.startsWith(file.getName()) && name.endsWith(".gz");
-            }
-        });
-        if (listFiles.length > this.maxBackups) {
-            final List<File> fileList = Arrays.asList(listFiles);
-            Collections.sort(fileList);
-            Collections.reverse(fileList);
-            final List<File> deleteList = fileList.subList(this.maxBackups, fileList.size());
-            this.deleteFiles(deleteList);
-        }
-    }
-
-    /**
-     * Deletes the List of Files
-     * @param files List of Files
-     */
-    private void deleteFiles(final List<File> files) {
-        for (File file : files) {
-            System.out.println("Delete File " + file.getName());
-            file.delete();
-        }
-    }
-
-    /**
-     * Zips the old logfile with the requested zip format
-     * @param suffix the old suffix from the rotated logfile
-     */
-    private void zippingOldLog(final String suffix) {
-        System.out.println("zippingOldLog in format " + this.zipFormat);
-        final File file = new File(this.getFile() + suffix);
-        if (!file.exists() || !file.isFile()) {
-            reportError("File to zip not existing " + file.getAbsolutePath(),
-                    new FileNotFoundException(file.getAbsolutePath()), ErrorManager.GENERIC_FAILURE);
-            return;
-        }
-        switch (this.zipFormat) {
-            case GZIP:
-                System.out.println("zippingOldLogWithGzip");
-                this.zippingOldLogWithGzip(file);
-                break;
-            case ZIP:
-                System.out.println("zippingOldLogWithZip");
-                this.zippingOldLogWithZip(file);
-                break;
-        }
-    }
-
-    /**
-     * Zipping the File with Gzip
-     * @param file file to gzip
-     */
-    private void zippingOldLogWithGzip(final File file) {
-        if (!file.exists()) {
-            reportError("File to zip not existing " + file.getAbsolutePath(),
-                    new FileNotFoundException(file.getAbsolutePath()), ErrorManager.GENERIC_FAILURE);
-            return;
-        }
-        byte[] buffer = new byte[1024];
-        try {
-            GZIPOutputStream gzos = new GZIPOutputStream(new FileOutputStream(file.getAbsolutePath() + ".gz"));
-            FileInputStream in = new FileInputStream(file);
-            int len;
-            while ((len = in.read(buffer)) > 0) {
-                gzos.write(buffer, 0, len);
-            }
-            in.close();
-            gzos.finish();
-            gzos.close();
-            file.delete();
-        } catch(IOException ex) {
-            reportError("Could not write gzipped File", ex, ErrorManager.WRITE_FAILURE);
-        }
-    }
-
-    /**
-     * Zipping the File with Zip
-     * @param file file to zip
-     */
-    private void zippingOldLogWithZip(final File file) {
-        if (!file.exists()) {
-            reportError("File to zip not existing " + file.getAbsolutePath(),
-                    new FileNotFoundException(file.getAbsolutePath()), ErrorManager.GENERIC_FAILURE);
-            return;
-        }
-        byte[] buffer = new byte[1024];
-        try {
-            ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(file.getAbsolutePath() + ".zip"));
-            final ZipEntry zipEntry = new ZipEntry(file.getName());
-            zos.putNextEntry(zipEntry);
-            FileInputStream in = new FileInputStream(file);
-            int len;
-            while ((len = in.read(buffer)) > 0) {
-                zos.write(buffer, 0, len);
-            }
-            in.close();
-            zos.closeEntry();
-            zos.close();
-            file.delete();
-        } catch(IOException ex) {
-            reportError("Could not write zipped File", ex, ErrorManager.WRITE_FAILURE);
-        }
-    }
 
     public ZipFormat getZipFormat() {
         return zipFormat;
@@ -214,5 +101,138 @@ public class ZippedPeriodicRotatingFileHandler extends PeriodicRotatingFileHandl
     public void setFileName(final String fileName) throws FileNotFoundException {
         System.out.println("setFileName: " + fileName);
         super.setFileName(fileName);
+    }
+
+    /**
+     * Zipping oldLog and deletes files
+     * non blocking for the Logging Thread
+     */
+    class JobRunner implements Runnable {
+
+        private String oldSuffix;
+
+        public JobRunner(final String oldSuffix) {
+            this.oldSuffix = oldSuffix;
+        }
+
+        @Override
+        public void run() {
+            this.zippingOldLog(this.oldSuffix);
+            this.checkMaxBackups();
+        }
+
+        /**
+         * Check if the maximum number of backups is reached and delete the files that are too much
+         */
+        private void checkMaxBackups() {
+            if (getMaxBackups() < 1) {
+                return;
+            }
+            final File file = getFile();
+            final File[] listFiles = file.getParentFile().listFiles(new FilenameFilter() {
+                @Override
+                public boolean accept(final File dir, final String name) {
+                    return name.startsWith(file.getName()) && name.endsWith(".gz");
+                }
+            });
+            if (listFiles.length > getMaxBackups()) {
+                final List<File> fileList = Arrays.asList(listFiles);
+                Collections.sort(fileList);
+                Collections.reverse(fileList);
+                final List<File> deleteList = fileList.subList(getMaxBackups(), fileList.size());
+                this.deleteFiles(deleteList);
+            }
+        }
+
+        /**
+         * Deletes the List of Files
+         * @param files List of Files
+         */
+        private void deleteFiles(final List<File> files) {
+            for (File file : files) {
+                System.out.println("Delete File " + file.getName());
+                file.delete();
+            }
+        }
+
+        /**
+         * Zips the old logfile with the requested zip format
+         * @param suffix the old suffix from the rotated logfile
+         */
+        private void zippingOldLog(final String suffix) {
+            System.out.println("zippingOldLog in format " + zipFormat);
+            final File file = new File(getFile() + suffix);
+            if (!file.exists() || !file.isFile()) {
+                reportError("File to zip not existing " + file.getAbsolutePath(),
+                        new FileNotFoundException(file.getAbsolutePath()), ErrorManager.GENERIC_FAILURE);
+                return;
+            }
+            switch (zipFormat) {
+                case GZIP:
+                    System.out.println("zippingOldLogWithGzip");
+                    this.zippingOldLogWithGzip(file);
+                    break;
+                case ZIP:
+                    System.out.println("zippingOldLogWithZip");
+                    this.zippingOldLogWithZip(file);
+                    break;
+            }
+        }
+
+        /**
+         * Zipping the File with Gzip
+         * @param file file to gzip
+         */
+        private void zippingOldLogWithGzip(final File file) {
+            if (!file.exists()) {
+                reportError("File to zip not existing " + file.getAbsolutePath(),
+                        new FileNotFoundException(file.getAbsolutePath()), ErrorManager.GENERIC_FAILURE);
+                return;
+            }
+            byte[] buffer = new byte[1024];
+            try {
+                GZIPOutputStream gzos = new GZIPOutputStream(new FileOutputStream(file.getAbsolutePath() + ".gz"));
+                FileInputStream in = new FileInputStream(file);
+                int len;
+                while ((len = in.read(buffer)) > 0) {
+                    gzos.write(buffer, 0, len);
+                }
+                in.close();
+                gzos.finish();
+                gzos.close();
+                file.delete();
+            } catch(IOException ex) {
+                reportError("Could not write gzipped File", ex, ErrorManager.WRITE_FAILURE);
+            }
+        }
+
+        /**
+         * Zipping the File with Zip
+         * @param file file to zip
+         */
+        private void zippingOldLogWithZip(final File file) {
+            if (!file.exists()) {
+                reportError("File to zip not existing " + file.getAbsolutePath(),
+                        new FileNotFoundException(file.getAbsolutePath()), ErrorManager.GENERIC_FAILURE);
+                return;
+            }
+            byte[] buffer = new byte[1024];
+            try {
+                ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(file.getAbsolutePath() + ".zip"));
+                final ZipEntry zipEntry = new ZipEntry(file.getName());
+                zos.putNextEntry(zipEntry);
+                FileInputStream in = new FileInputStream(file);
+                int len;
+                while ((len = in.read(buffer)) > 0) {
+                    zos.write(buffer, 0, len);
+                }
+                in.close();
+                zos.closeEntry();
+                zos.close();
+                file.delete();
+            } catch(IOException ex) {
+                reportError("Could not write zipped File", ex, ErrorManager.WRITE_FAILURE);
+            }
+        }
     }
 }
